74ddef619b9d9e56c7d26010dd581f7364d31b7f
seifzogheibi <146748894+seifzogheibi@users.noreply.github.com>
2023-12-01 11:54:26 +0000

    task3

--- Coursework3/FitnessDataSorter.c
+++ Coursework3/FitnessDataSorter.c
@@ -10,6 +10,15 @@ typedef struct {
 } FitnessData;
 
 // Function to tokenize a record
+
+int sortDescending(const void *a, const void *b) {
+    // To sort in descending order, return a negative value if b should come before a
+    // Return a positive value if a should come before b
+    // Return 0 if they are equal (order doesn't matter for equal elements)
+    return ((FitnessData *)b)->steps - ((FitnessData *)a)->steps;
+
+}
+
 void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *steps) {
     char *ptr = strtok(record, &delimiter);
     if (ptr != NULL) {
@@ -28,18 +37,54 @@ void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *s
 
 int main() {
 
-#define buffer_size 900
-char line[buffer_size];
-char filename [] = "FitnessData_2023.csv";
-    
-printf("Enter filename: ");
-            fgets(line, buffer_size, stdin);
-            sscanf(line, " %s ", filename);
+    #define buffer_size 900
+    char line[buffer_size];
+    char filename [] = "FitnessData_2023.csv";
+    FitnessData Fitness [900];
+    char strsteps[900];
+    FILE*file = NULL;
+    int numberOfElements = 0;
 
-            FILE *input = fopen(filename, "r");
-            if (!input)
-            {
-                printf("Error: Invalid file.\n");
-                return 1;
-            }
+    printf("Enter filename: ");
+    fgets(line, buffer_size, stdin);
+    sscanf(line, " %s ", filename);
+
+    FILE *input = fopen(filename, "r");
+    if (!input)
+    {
+        printf("Error: Invalid file.\n");
+        return 1;
+    }
+
+    int counter = 0;
+
+    while (fgets(line, buffer_size, input))
+    {
+        tokeniseRecord(line, ',', Fitness[counter].date, Fitness[counter].time, &Fitness[counter].steps);
+        counter++;
+    }
+    numberOfElements = counter;
+    printf("File successfully loaded.\n");
+
+
+    qsort(Fitness, numberOfElements, sizeof(FitnessData), sortDescending);
+    
+// //writing to another file
+    FILE * fp;
+
+    fp = fopen ("file.txt", "w+");
+   
+    for (int i = 0; i < numberOfElements; i++) {
+        fprintf(fp, "%s\t%s\t%d\n", Fitness[i].date, Fitness[i].time, Fitness[i].steps);
+    }
+    fclose(fp);
+
+
+
+
+    return 0;
+
+
+
 }
+
